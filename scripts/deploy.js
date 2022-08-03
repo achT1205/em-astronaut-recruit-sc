a8bfc336a831cefc43995cfe2c61a907b3869364
achT1205 <achilletuglo12@gmail.com>
2022-08-03 02:10:30 +0200

    commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,7 +11,7 @@ main = async () => {
   recruit = await EMRecruit.deploy(
     "EMEMRecruit",
     "EMAST",
-    "ipfs://QmbjFSjTNvWDpx8DZDNA2PW5oosJ12HWs9XzmFjuKUQZcH/",
+    "ipfs://QmQHdnsheDFEP1eDV5XWkSgoXT8FrHrNxoKya2SKSBVCj4/",
     "https://ipfs.io/ipfs/QmSMrCFPr4UiB9CFvUQ2hFjk9u4UBJSnQ99z9NccBcDr2x/noreveal.png",
     systemAddress
   );
@@ -41,7 +41,7 @@ main = async () => {
   const recruitcontent = `module.exports=[
     "EMEMRecruit",
     "EMAST",
-    "ipfs://QmbjFSjTNvWDpx8DZDNA2PW5oosJ12HWs9XzmFjuKUQZcH/",
+    "ipfs://QmQHdnsheDFEP1eDV5XWkSgoXT8FrHrNxoKya2SKSBVCj4/",
     "https://ipfs.io/ipfs/QmSMrCFPr4UiB9CFvUQ2hFjk9u4UBJSnQ99z9NccBcDr2x/noreveal.png",
     "${systemAddress}"
   ];`;
